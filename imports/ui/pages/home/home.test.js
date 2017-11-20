5fd173445cbf89c2d9527af477bcb90ec388979b
Pablo Bertran <pablobertran@gmail.com>
2017-11-20 12:43:06 -0500

    - Client - Signup test - Client - Signin test

--- imports/ui/pages/home/home.test.js
+++ imports/ui/pages/home/home.test.js
@@ -11,4 +11,54 @@ import { $ } from 'meteor/jquery';
 
 describe('[Home Layout]', function () {
 
+    beforeEach(() => {
+        Template.registerHelper('_', key => key);
+    });
+
+    afterEach(() => {
+        Template.deregisterHelper('_');
+    });
+
+    if (Meteor.isClient) {
+
+        it('Should create a user and login', function () {
+            Accounts.createUser(demouser, function () {
+                let success = false;
+                Meteor.loginWithPassword(demouser.email, demouser.password, function (err) {
+                    if (err) {
+                        t.errorMessage.set(err.message);
+                    }
+                    success = true;
+                });
+
+                expect(success).to.equal(true);
+            });
+        });
+
+        it('Should login', () => {
+
+            const user = {
+                profile: {
+                    name: 'Pablo Bertran'
+                },
+                email: 'pablo.b@scopicsoftware.com',
+                password: 'test123'
+            }
+            let userCreated = true;
+
+            withRenderedTemplate('Signup', data, (el) => {
+                $(el).find('#name').val(user.profile.name);
+                $(el).find('#email').val(user.email);
+                $(el).find('#password').val(user.password);
+
+                $(el).find('.button.buttonBlue').click();
+
+                stop();
+
+
+                expect(Meteor.userId()).to.be.a('string');
+            });
+        });
+    }
+
 });
--- imports/ui/pages/signup/signup.test.js
+++ imports/ui/pages/signup/signup.test.js
@@ -1,14 +1,57 @@
+import {Factory} from 'meteor/dburles:factory';
+import {chai, expect} from 'meteor/practicalmeteor:chai';
+import {Template} from 'meteor/templating';
 import {Meteor} from 'meteor/meteor';
-import { Factory } from 'meteor/dburles:factory';
-import {Accounts} from 'meteor/accounts-base';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-import {chai, assert, expect} from 'meteor/practicalmeteor:chai';
-import { Template } from 'meteor/templating';
+import {$} from 'meteor/jquery';
+import {Random} from 'meteor/random';
+import {Messages} from "../../../api/messages/messages";
+import {Settings} from "../../../api/settings/settings";
 import {resetDatabase} from 'meteor/xolvio:cleaner';
-import { $ } from 'meteor/jquery';
-import { withRenderedTemplate } from '../../test-helpers';
 
+import {withRenderedTemplate} from '../../test-helpers.js';
+
+import '../../../api/users/server/publications';
 
 describe('[Signup Layout]', function () {
 
+    beforeEach(() => {
+        Template.registerHelper('_', key => key);
+    });
+
+    afterEach(() => {
+        Template.deregisterHelper('_');
+    });
+
+    if (Meteor.isClient) {
+
+        it('Should create a new user', () => {
+
+            const user = {
+                profile: {
+                    name: 'Pablo Bertran'
+                },
+                email: 'pablo.b@scopicsoftware.com',
+                password: 'test123'
+            }
+            let userCreated = true;
+
+            withRenderedTemplate('Signup', data, (el) => {
+                $(el).find('#name').val(user.profile.name);
+                $(el).find('#email').val(user.email);
+                $(el).find('#password').val(user.password);
+
+                $(el).find('.button.buttonBlue').click();
+
+                stop();
+
+                Account.createUser(user, function (err) {
+                    if (!err) {
+                        userCreated = false;
+                    }
+                });
+                expect(userCreated).to.equal(true);
+            });
+        });
+
+    }
 });
